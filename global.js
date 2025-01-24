07096fcd8b56ac15e782e92d7eabd47a4526be01
Sadrac Santacruz <70788009+SadracSantacruz@users.noreply.github.com>
2025-01-24 15:32:49 -0800

    added some comments but having some probelms changing the links on github

--- global.js
+++ global.js
@@ -1,9 +1,8 @@
 console.log("IT’S ALIVE!");
 
-// Detect if we're on the home page
 const ARE_WE_HOME = document.documentElement.classList.contains("home");
 
-// Pages array: Define your navigation structure
+// Links to all the pages
 const pages = [
   { url: "", title: "Home" },
   { url: "projects/", title: "Projects" },
@@ -12,16 +11,13 @@ const pages = [
   { url: "https://github.com/SadracSantacruz", title: "GitHub" },
 ];
 
-// Create a new <nav> element and add it to the <body> at the beginning
 const nav = document.createElement("nav");
 document.body.prepend(nav);
 
-// Iterate over the pages and create <a> elements dynamically
 for (let page of pages) {
   let url = page.url;
   let title = page.title;
 
-  // Adjust relative URLs for non-home pages
   url = !ARE_WE_HOME && !url.startsWith("http") ? "../" + url : url;
 
   // Create the <a> element
